d6190ee4c4131d98846b63ddc3f3973ab0b11f5a
Z3nJo <david.cortez.leal@gmail.com>
2026-05-02 13:53:54 -0400

    Adicion de resolucion de problema mediante programa de C++

---
+++ src/cpp/experimento.h
@@ -0,0 +1,8 @@
+#ifndef EXPERIMENTO_H
+#define EXPERIMENTO_H
+#include <string> 
+
+void ejecutarExperimento(int n, double prob);
+void ejecutarExperimentoArchivo(const std::string& ruta);
+
+#endif
